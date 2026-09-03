796a77c03b47b2e0dfef7a46d465607fc5eb34c5
kshetritej <tejghartikshetri@gmail.com>
2026-09-03 12:17:15 +0545

    fix: login and signup url

--- lib/site.ts
+++ lib/site.ts
@@ -2,5 +2,5 @@ export const SITE_NAME = "TripEleven";
 export const SITE_URL = "https://tripeleven.com";
 export const APP_URL = "https://app.tripeleven.com";
 export const SIGNUP_URL = `${APP_URL}/signup`;
-export const LOGIN_URL = `${APP_URL}/admin`;
+export const LOGIN_URL = `${APP_URL}/login`;
 export const CONTACT_EMAIL = "hello@tripeleven.com";
